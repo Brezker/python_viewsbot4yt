0dc336d5a80a7e9a2566ca2f03fa1ee5227bd544
brezker <brezker10@gmail.com>
2022-08-29 19:23:09 -0500

    Abre las ventanas

---
+++ viewsbot4yt.py
@@ -0,0 +1,4 @@
+import webbrowser
+webbrowser.open("https://www.youtube.com/watch?v=EBC5F_JaEMs&t=1s", new=1, autoraise=True)
+webbrowser.open("https://www.youtube.com/watch?v=EBC5F_JaEMs&t=1s", new=1, autoraise=True)
+
