655005018de9a93c4ea510d497d2823cfb7773a1
brezker <brezker10@gmail.com>
2022-08-29 19:51:13 -0500

    Cierra todas las instancias de firefox

--- viewsbot4yt.py
+++ viewsbot4yt.py
@@ -1,4 +1,10 @@
+import os
 import webbrowser
+import time
+
 webbrowser.open("https://www.youtube.com/watch?v=EBC5F_JaEMs&t=1s", new=1, autoraise=True)
 webbrowser.open("https://www.youtube.com/watch?v=EBC5F_JaEMs&t=1s", new=1, autoraise=True)
 
+time.sleep(10)
+
+os.system('killall -KILL firefox')
