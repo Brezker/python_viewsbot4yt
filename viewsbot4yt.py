9fa20f642181489fcddeb5f2f14e8b5b15099b8a
brezker <brezker10@gmail.com>
2022-08-30 21:39:49 -0500

    Ya funciona pero solo en Linux y con Firefox

--- viewsbot4yt.py
+++ viewsbot4yt.py
@@ -2,9 +2,14 @@ import os
 import webbrowser
 import time
 
-webbrowser.open("https://www.youtube.com/watch?v=EBC5F_JaEMs&t=1s", new=1, autoraise=True)
-webbrowser.open("https://www.youtube.com/watch?v=EBC5F_JaEMs&t=1s", new=1, autoraise=True)
+while(0==0):
+    webbrowser.open("https://www.youtube.com/watch?v=EBC5F_JaEMs&t=1s", new=1, autoraise=False)
+    webbrowser.open("https://www.youtube.com/watch?v=9c2vhVl24Wk&t=3s", new=1, autoraise=False)
+    webbrowser.open("https://www.youtube.com/watch?v=TS7bK9_wv3g&t=1s", new=1, autoraise=False)
+    webbrowser.open("https://www.youtube.com/watch?v=6HLIOeGSdUY&t=1s", new=1, autoraise=False)
+    webbrowser.open("https://www.youtube.com/watch?v=oUmEsN6muCc&t=113s", new=1, autoraise=False)
+    webbrowser.open("https://www.youtube.com/watch?v=hdxiH4Co4qc&t=66s", new=1, autoraise=False)
 
-time.sleep(10)
+    time.sleep(200)
 
-os.system('killall -KILL firefox')
+    os.system('killall -KILL firefox')
