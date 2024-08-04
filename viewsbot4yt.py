0ba895c99595abd895a71e6bbf7961ffcfcf87b5
Brezker <38386181+Brezker@users.noreply.github.com>
2024-08-04 17:20:43 -0600

    Update viewsbot4yt.py

--- viewsbot4yt.py
+++ viewsbot4yt.py
@@ -7,9 +7,8 @@ while(0==0):
     webbrowser.open("https://www.youtube.com/watch?v=9c2vhVl24Wk&t=3s", new=1, autoraise=False)
     webbrowser.open("https://www.youtube.com/watch?v=TS7bK9_wv3g&t=1s", new=1, autoraise=False)
     webbrowser.open("https://www.youtube.com/watch?v=6HLIOeGSdUY&t=1s", new=1, autoraise=False)
-    webbrowser.open("https://www.youtube.com/watch?v=oUmEsN6muCc&t=113s", new=1, autoraise=False)
     webbrowser.open("https://www.youtube.com/watch?v=hdxiH4Co4qc&t=66s", new=1, autoraise=False)
-    #webbrowser.open("Your YT video URL", new=1, autoraise=False)
+    #webbrowser.open("Your.YT.video.URL=number_of_seconds_that_want_to_start", new=1, autoraise=False)
 
     time.sleep(200)
 
